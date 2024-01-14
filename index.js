b9f17af499d85ecd7c10acd6ad8b2496b87cd9db
Alex Njoroge <alex.njoroge@student.moringaschool.com>
2024-01-14 15:40:45 -0800

    doneWithAssignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Write your solution in this file!
+let employee = {
+    name: 'Rashford',            
+    streetAddress: 'manchester' 
+  };
+
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    
+    const updatedEmployee = { ...employee };
+    
+    
+    updatedEmployee[key] = value;
+  
+    
+    return updatedEmployee;
+  }
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    
+    employee[key] = value;
+  
+    return employee;
+  }
+
+  function deleteFromEmployeeByKey(employee, key) {
+    
+    const employeeClone = { ...employee };
+  
+    
+    if (employeeClone.hasOwnProperty(key)) {
+      
+      delete employeeClone[key];
+    }
+  
+    
+    return employeeClone;
+  }
+  
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    
+    if (employee.hasOwnProperty(key)) {
+      
+      delete employee[key];
+    }
+  
+    
+    return employee;
+}
